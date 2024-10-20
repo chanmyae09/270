44aecedbe536f90e9f2a0ba2beb37a732e4b55de
UlizesR <fulizesrp@gmail.com>
2024-10-19 19:03:38 -0700

    added calc

---
+++ calculator/calculator.cpp
@@ -0,0 +1,21 @@
+#include "calculator.hpp"
+
+double Calculator::Add (const double &a, const double &b)
+{
+    return a + b;
+}
+
+double Calculator::Subtract (const double &a, const double &b)
+{
+    return a - b;
+}
+
+double Calculator::Multiply (const double &a, const double &b)
+{
+    return a * b;
+}
+
+double Calculator::Divide (const double &a, const double &b)
+{
+    return a / b;
+}
---
+++ calculator/calculator.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+class Calculator
+{
+public:
+    double Add (const double &a, const double &b);
+    double Subtract (const double &a, const double &b);
+    double Multiply (const double &a, const double &b);
+    double Divide (const double &a, const double &b);
+};
+
